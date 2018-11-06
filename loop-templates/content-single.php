c52a953700a9d62b97bce034cae072addb9fdf5b
jazinheira <jazinheira@users.noreply.github.com>
2018-11-06 13:35:30 -0500

    Fix featured image in single posts

--- loop-templates/content-single.php
+++ loop-templates/content-single.php
@@ -24,7 +24,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 	</header><!-- .entry-header -->
 
-	<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
+	<?php echo get_the_post_thumbnail( $post->ID, 'large card card-img-top mb-3' ); ?>
 
 	<div class="entry-content">
 
