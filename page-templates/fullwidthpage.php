d60dc2a0f68e01f314be22f673daa091b40535b2
jazinheira <jazinheira@users.noreply.github.com>
2018-11-07 13:54:14 -0500

    Add card to fullwidth templates

--- page-templates/fullwidthpage.php
+++ page-templates/fullwidthpage.php
@@ -25,7 +25,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 			<div class="col-md-12 content-area" id="primary">
 
-				<main class="site-main" id="main" role="main">
+				<main class="site-main card" id="main" role="main">
 
 					<?php while ( have_posts() ) : the_post(); ?>
 
